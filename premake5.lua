03bed9b4039a808231d6f61e01e86b508bd2cbb8
Can Karka <53074877+Cankar001@users.noreply.github.com>
2021-11-26 15:36:04 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -5,7 +5,7 @@ project "FontAwesomeCppHeaders"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-	staticruntime "on"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
